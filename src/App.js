fea8b01f15c6b10ccc281c300ad036d163684746
isabellagh <isabellagh88@gmail.com>
2022-05-09 13:16:37 -0400

    header icons and footer name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Shop Soccer</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
